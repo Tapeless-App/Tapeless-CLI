91434211a16073274365b7ec0f6e3c6e02860ceb
Douglas Reiser <d.reiser@outlook.de>
2024-10-04 15:02:33 +0200

    Fixed date parsing bug in new project creation

--- cmd/projects/add.go
+++ cmd/projects/add.go
@@ -89,7 +89,7 @@ var addProjectsCmd = &cobra.Command{
 			return
 		}
 
-		err = util.VerifyStartBeforeEnd(projectStart, projectEnd, "2006-01-02T15:04:05.000Z")
+		err = util.VerifyStartBeforeEnd(projectStart, projectEnd, "2006-01-02")
 
 		if err != nil {
 			fmt.Println("Error:", err)
