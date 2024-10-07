fd78e5746a6aa6ce7fa6929eddddbd79faf39387
Douglas Reiser <d.reiser@outlook.de>
2024-10-07 18:04:46 +0200

    Upgraded local version

--- env/env.go
+++ env/env.go
@@ -4,5 +4,5 @@ var (
 	ApiURL            = "http://localhost:4000/cli/v1"
 	WebURL            = "http://localhost:5173"
 	LoginCallbackPort = "8123"
-	Version           = "0.0.10"
+	Version           = "0.0.13"
 )
